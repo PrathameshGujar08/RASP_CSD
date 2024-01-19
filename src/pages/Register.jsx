06044cf5032e30af206fdfb38a01976780f720b3
riyanshi <goyalriyanshi26@gmail.com>
2024-01-19 18:19:46 +0530

    checking if code works

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,6 @@ import React, {useState ,useEffect} from 'react'
 import { Link , useNavigate} from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify'; 
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify'; 
-import 'react-toastify/dist/ReactToastify.css';
 // import { registerRoute } from '../utils/APIRoutes';
 import axios from 'axios'
 function Register() {
